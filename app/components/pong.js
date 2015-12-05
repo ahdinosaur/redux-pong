92ce7bee3da017f704b4c9a606993289ff2e7790
Michael Williams <michael.williams@enspiral.com>
2015-12-05 21:58:33 +1300

    simple render of state

--- app/components/pong.js
+++ app/components/pong.js
@@ -2,10 +2,29 @@ const React = require('react')
 
 const Pong = React.createClass({
   render: function () {
-    return <svg>
-      <rect />
-      <circle />
-      <rect />
+    const { computer, player, ball } = this.props
+
+    return <svg viewBox="0 -1 1 2">
+      <rect
+        className="computer"
+        height={0.05}
+        width={0.2}
+        x={computer.position}
+        y={-1}
+      />
+      <circle
+        className="ball"
+        r={0.05}
+        cx={0.5}
+        cy={0}
+      />
+      <rect
+        className="player"
+        height={0.05}
+        width={0.2}
+        x={player.position}
+        y={0.95}
+      />
     </svg>
   }
 })
